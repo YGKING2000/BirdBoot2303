934b79effdbfc90545a1587f7a6192f5a16237f8
YGKING <hrd18960706057@163.com>
2023-04-19 20:11:22 +0800

    V13

---
+++ V12/src/main/java/com/birdboot/core/ClientHandler.java
@@ -0,0 +1,49 @@
+package com.birdboot.core;
+
+import com.birdboot.http.EmptyRequestException;
+import com.birdboot.http.HttpServletRequest;
+import com.birdboot.http.HttpServletResponse;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * @Description 客户端处理器
+ * @ClassName ClientHandler
+ * @Author YGKING e-mail:hrd18960706057@163.com
+ * @Date 2023/04/17 10:21
+ * @Version 1.0
+ */
+public class ClientHandler implements Runnable {
+    private final Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // 创建HTTP服务器请求对象
+            HttpServletRequest request = new HttpServletRequest(socket);
+
+            // 1.创建HTTP服务器响应对象
+            HttpServletResponse response = new HttpServletResponse(socket);
+            // 2.处理请求
+            DispatcherServlet servlet = new DispatcherServlet();
+            servlet.service(request, response);
+            // 3.发送响应
+            response.response();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (EmptyRequestException e) {
+
+        }finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ V13/src/main/java/com/birdboot/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.birdboot.controller;
+
+import com.birdboot.http.HttpServletRequest;
+import com.birdboot.http.HttpServletResponse;
+
+/**
+ * @Description User
+ * @ClassName UserController
+ * @Author YGKING e-mail:hrd18960706057@163.com
+ * @Date 2023/04/19 17:40
+ * @Version 1.0
+ */
+public class UserController {
+    public void reg(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("正在注册...");
+    }
+}
